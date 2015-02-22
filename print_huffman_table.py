00341ea65367d0f227f95b07a0dd47960646990b
Julia Evans <julia@jvns.ca>
2015-02-22 15:21:30 -0500

    Fix bug in Huffman code generation

--- print_huffman_table.py
+++ print_huffman_table.py
@@ -17,7 +17,7 @@ def print_codes(lengths):
     last_length = None
     for char, length in lengths:
         if last_code is None:
-            code = 1 << length
+            code = 0
         else:
             if last_length == length:
                 code = last_code + 1
@@ -28,7 +28,7 @@ def print_codes(lengths):
                 code = (last_code + 1) << 1
         last_code = code
         last_length = length
-        print "%06s" % repr(char), bin(code)[2:]
+        print "%04s" % repr(char), bin(code)[2:].zfill(length)
 
 
 def read_lengths(infgen_output):

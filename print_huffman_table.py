8075f174a579885f5a47a4cf412b3df89c4fd7a5
Punk Rider <kornos@users.noreply.github.com>
2016-10-28 17:31:02 +0300

    Multiple problems fixed
    Problems: It doesn't support multiple blocks in deflate streams,it wrongly assumes that smaller code lengths occur for symbol code<256, list sorting just messes things up , mixing different code lengths from different blocks

--- print_huffman_table.py
+++ print_huffman_table.py
@@ -2,44 +2,91 @@ import sys
 
 
 def print_codes(lengths):
-    """
-    Print a Huffman tree given pairs of characters and Huffman lengths.
-    For instance:
-    a, 3
-    b, 3
-    c, 4
-    will output
-    100
-    101
-    1100
-    """
-    last_code = None
-    last_length = None
-    for char, length in lengths:
-        if last_code is None:
-            code = 0
-        else:
-            if last_length == length:
-                code = last_code + 1
-            else:
-                # Figure out what the first code for each bit-length would
-                # be. This is one more than the last code of the previous
-                # bit length, left-shifted once.
-                code = (last_code + 1) << 1
-        last_code = code
-        last_length = length
-        print "%04s" % repr(char), bin(code)[2:].zfill(length)
+
+    # MAX_BITS = 15 -> maximum number of bits for code lengths
+    # bl_count stores how many codes are found for each symbol ASCII code
+    min_length = 15 
+    bl_count = [0] * 16 
+    for lines in lengths:
+         bl_count[lines[1]-1]+=1 
+         if min_length > lines[1]:
+                min_length = lines[1]
+
+
+    # next_code contains the first code at each length change(eg. from 4 to 5 bits)
+    next_code = [0] * 16
+    code = 0
+
+    for bits in range(1,16):
+        code = ( code + bl_count[ bits - 1 ] ) << 1
+        if  bl_count[ bits ]:
+                next_code[ bits ] = code
+
+    print "Huffman tree code lengths:"
+    print "========================="
+    for bits in range(min_length,16):
+        print bits,bin(next_code[bits-1])[2:].zfill(min_length)
+    print "========================="
+
+
+
+    # There are 2 sorting ways: by Symbol ASCII CODE or by somb code length.
+    # Only one method can be activated in the code.
+
+    '''
+    # Sort by Symbol ASCII code
+    for lines in lengths:
+        bits = lines[1]
+        if lines[0] < 256:
+                print "%04s" % repr(chr(lines[0])), bin(next_code[bits-1])[2:].zfill(min_length)
+        next_code[bits-1] +=1
+    '''
+
+    # Sort by symbol code length
+    for current_bits in range(min_length,16):
+        for lines in lengths:
+                bits = lines[1]
+                if lines[0] < 256 and bits == current_bits:
+                        print "%06s" % repr(chr(lines[0])), bin(next_code[bits-1])[2:].zfill(min_length)
+                if bits == current_bits:
+                        next_code[bits-1] +=1
 
 
 def read_lengths(infgen_output):
-    lines = (l for l in infgen_output if 'litlen' in l)
-    lines = (l.split() for l in lines)
-    lines = ((int(code), int(length)) for _, code, length in lines)
-    # Filter out anything where the code isn't ascii
-    lines = ((chr(code), length) for code, length in lines if code < 256)
-    return list(lines)
+
+    # DEFLATE stream contains one or more blocks . Process one block at a time.
+    # Otherwise codes and lengths get mangled.
+    # Different blocks have different code lengths.
+
+    blockslist = []
+    blocklines = []
+    for line in infgen_output:
+	if line.startswith('end'):  # All blocks should have an "end" directive , otherwise this will fail
+		blockslist.append (blocklines)	
+		blocklines = []
+	elif 'litlen' in line:
+		split = line.split()
+		blocklines.append( (int(split[1]),int(split[2])) )
+    return blockslist
+
+
 
 if __name__ == "__main__":
-    lengths = read_lengths(sys.stdin)
-    lengths.sort(key=lambda x: x[1])
-    print_codes(lengths)
+    ''' 
+    DEFLATE algorithm dissector
+    See: https://jvns.ca/blog/2015/02/22/how-gzip-uses-huffman-coding/
+	 http://www.infinitepartitions.com/art001.html
+
+   '''
+
+    blockslist = read_lengths(sys.stdin)
+    block_no = 0
+
+    for blocks in blockslist:
+	block_no += 1
+	print "Block number = ", block_no
+	print "========================="
+        print_codes(blocks)
+	print "\n"
+    
+    print "Number of blocks = ", block_no
